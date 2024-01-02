96863dc0985d6b676c2850bcd56d5dbf7cd51474
Raphael Zanarelli <raphaelzanarelli@outlook.com>
2024-01-02 00:06:55 -0300

    stream kafka

--- demo-producer/src/main/java/com/example/demoproducer/DemoController.java
+++ demo-producer/src/main/java/com/example/demoproducer/DemoController.java
@@ -1,16 +1,12 @@
 package com.example.demoproducer;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.stream.binder.kafka.streams.InteractiveQueryService;
 import org.springframework.cloud.stream.function.StreamBridge;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
@@ -19,7 +15,7 @@ public class DemoController {
     @Autowired
     private StreamBridge streamBridge;
 
-    public DemoController(StreamBridge streamBridge, InteractiveQueryService interactiveQueryService) {
+    public DemoController(StreamBridge streamBridge) {
         this.streamBridge = streamBridge;
     }
 
