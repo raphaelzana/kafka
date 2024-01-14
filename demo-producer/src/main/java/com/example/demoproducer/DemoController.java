3b7cdadcf0f44cd9597f8a5a8be667c1564cbd29
Raphael Zanarelli <raphaelzanarelli@outlook.com>
2024-01-14 18:15:12 -0300

    messageKey, Dlq Exceptions, etc

--- demo-producer/src/main/java/com/example/demoproducer/DemoController.java
+++ demo-producer/src/main/java/com/example/demoproducer/DemoController.java
@@ -1,11 +1,14 @@
 package com.example.demoproducer;
 
+import java.util.UUID;
 import java.util.function.Supplier;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.stream.function.StreamBridge;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpStatus;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.kafka.support.converter.KafkaMessageHeaders;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -20,15 +23,13 @@ public class DemoController {
     @Autowired
     private StreamBridge streamBridge;
 
-    public DemoController(StreamBridge streamBridge) {
-        this.streamBridge = streamBridge;
-    }
-
     @RequestMapping
     @ResponseStatus(HttpStatus.ACCEPTED)
     public void publish(@RequestBody Person body){        
         streamBridge.send("myPartitionedTopic", MessageBuilder.withPayload(body)
                     .setHeader("partitionKey", body)
+                    .setHeader("messageKey", UUID.randomUUID().toString())
+                    .setHeader(KafkaHeaders.KEY, UUID.randomUUID().toString())
                     .build());
     }    
 }
